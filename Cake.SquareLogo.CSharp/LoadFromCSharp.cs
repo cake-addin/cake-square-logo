ea7a6f5057733167cf094ea3ad9a540a0681d1cc
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-09-08 19:25:06 +0700

    Try to load extension from C#

---
+++ Cake.SquareLogo.CSharp/LoadFromCSharp.cs
@@ -0,0 +1,17 @@
+﻿using Cake.Core;
+using Cake.SquareLogo.LogoAlias;
+
+namespace Cake.SquareLogo.CSharp
+{
+    public class LoadFromCSharp
+    {
+        public LoadFromCSharp()
+        {
+        }
+
+        public void Ext(ICakeContext context)
+        {
+            context.CreateLogo("Hello", "Hello.png");
+        }
+    }
+}
